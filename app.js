442c2a2ac0aa3b190fb201ae66f5a50422d58d30
WesleyPierrotte <wesley_perrotte@yahoo.com.br>
2023-01-05 21:30:40 -0300

    voltando para trás

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
-// teste de subida de arquivo
-console.log("Mais uma mensagem para ver o que vai acontecer");
